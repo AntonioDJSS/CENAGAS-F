34b8f1837271d4d0501fd22ddf5777d595361956
Antonio Sanchez <adjss.dev@gmail.com>
2022-12-21 23:50:38 -0600

    Actualizacion de la pagina principal

--- src/components/index-page/Incentives.jsx
+++ src/components/index-page/Incentives.jsx
@@ -2,21 +2,21 @@ const incentives = [
   {
     name: "Inicia tú prueba gratuita",
     description:
-      "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+      "Automatiza tus procesos con nosotros, inicia la prueba gratuita y genera constancias DC-3.",
     imageSrc: "bg-[url('https://imgur.com/VrhforX.png')]",
     link: "Iniciar prueba",
   },
   {
-    name: "Contrata el plan associate",
+    name: "Contrata el plan Associate",
     description:
-      "Our AI chat widget is powered by a naive series of if/else statements. Guaranteed to irritate.",
+      "¿Quieres generar constancias DC-3 sin ninguna limitación? Este es el plan perfecto para tí.",
     imageSrc: "bg-[url('https://imgur.com/YWWE7VP.png')]",
     link: "Dirigete a los planes",
   },
   {
-    name: "Destaca con el plan professional",
+    name: "Destaca con el plan Professional",
     description:
-      "Look how fast that cart is going. What does this mean for the actual experience? I don't know.",
+      "¿Te encuentras a otro nivel? Contrata el plan Professional y genera constancias DC-3 de forma masiva.",
     imageSrc: "bg-[url('https://imgur.com/20XQjXe.png')]",
     link: "Quiero saber más",
   },
@@ -31,7 +31,7 @@ const incentives2 = [
     link: "Leer más",
   },
   {
-    name: "Contrata el plan associate",
+    name: "Contrata el plan Associate",
     description:
       "Our AI chat widget is powered by a naive series of if/else statements. Guaranteed to irritate.",
     imageSrc: "bg-[url('https://imgur.com/YWWE7VP.png')]",
@@ -81,14 +81,15 @@ export default function Incentives() {
               >
                 <div className="mt-3 sm:mt-0 lg:mt-3 lg:ml-0">
                   <h3 className="text-xl font-bold text-gray-900">
-                    Generador de constancias empresariales.
+                    Generador de constancias Enterprise.
                   </h3>
                   <p className="mt-2 text-md text-gray-700">
-                    "It's not actually free we just price it into the products.
-                    Someone's paying for it, and it's not us."
+                    ¿Quieres ampliar tú experiencia? El creador de constancias
+                    Enterprise es para tí, complemento ideal para el generador
+                    DC-3.
                   </p>
                   <p className="mt-2 text-xs font-bold text-black hover:underline">
-                    Leer más
+                    Consultar ejemplos
                   </p>
                 </div>
               </div>
@@ -103,8 +104,8 @@ export default function Incentives() {
                   </h3>
                   <p className="mt-2 text-md text-gray-700">
                     ¿Quieres mejorar la accesibilidad a tú información? Vincula
-                    tu cuenta de Google Drive para generar respaldos
-                    dinamicamente.
+                    tu cuenta de Google Drive para generar los respaldos de tus
+                    constancias dinamicamente.
                   </p>
                   <p className="mt-2 text-xs font-bold text-black hover:underline">
                     Descubre como
